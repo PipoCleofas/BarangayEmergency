90598c652e1626a60819a351859a00464dd1dad4
4reuminct <tacujantrish@gmail.com>
2024-09-10 17:26:53 +0800

    website update

--- website/web-app/src/pages/AdminLogin.tsx
+++ website/web-app/src/pages/AdminLogin.tsx
@@ -1,4 +1,5 @@
 
+import logo from '../pictures/logo.gif'; 
 
 
 export default function AdminLogin() {
@@ -7,9 +8,29 @@ export default function AdminLogin() {
 
 
   return (
-    <div>
-        AdminLogin
+    <div style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+        <img src={logo} alt="logo" />
+        <title>Admin Login</title>
+      <div className="login-container" style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+          <h2>Admin Login</h2>
+          
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+            
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              
+            />
+            <button type="submit">Sign In</button>
+          </form>
+      </div>
+            
     </div>
   )
-
 }
+
+
