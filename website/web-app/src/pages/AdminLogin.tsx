c2b170ad7c2439f5b1e59f5bbb82054048e28b63
4reuminct <tacujantrish@gmail.com>
2024-09-10 21:42:02 +0800

    web update

--- website/web-app/src/pages/AdminLogin.tsx
+++ website/web-app/src/pages/AdminLogin.tsx
@@ -8,29 +8,26 @@ export default function AdminLogin() {
 
 
   return (
-    <div style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-        <img src={logo} alt="logo" />
-        <title>Admin Login</title>
-      <div className="login-container" style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-          <h2>Admin Login</h2>
-          
-          <form>
-            <input
-              type="text"
-              placeholder="Username"
-            
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              
-            />
-            <button type="submit">Sign In</button>
-          </form>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div className="login-container" style={{ backgroundColor: 'white', padding: '20px', borderRadius: '10px', width: '300px' }}>
+        <img src={logo} alt="logo" style={{ margin: '20px auto', alignSelf: 'center' }} />
+        <h2>LOGIN</h2>
+        <form>
+          <label>Username:</label>
+          <input
+            type="text"
+            placeholder="Username"
+            style={{ width: '100%', padding: '10px', marginBottom: '10px' }}
+          />
+          <label>Password:</label>
+          <input
+            type="password"
+            placeholder="Password"
+            style={{ width: '100%', padding: '10px', marginBottom: '20px' }}
+          />
+          <button type="submit" style={{ width: '100%', padding: '10px', backgroundColor: '#4CAF50', color: '#fff', border: 'none' }}>Sign In</button>
+        </form>
       </div>
-            
     </div>
   )
 }
-
-
