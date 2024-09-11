6c9c73cf64854f30fb24936c3ee007a53be6788d
4reuminct <tacujantrish@gmail.com>
2024-09-12 00:15:21 +0800

    web update

--- website/web-app/src/pages/AdminDashboard.tsx
+++ website/web-app/src/pages/AdminDashboard.tsx
@@ -6,10 +6,33 @@ export default function AdminDashboard() {
 
 
 
-    return (
-      <div>
-          AdminDashboard
+  return (
+    <div className="admin-dashboard">
+      <div className="left-side">
+        <div className="nav">
+          <ul>
+            <li>
+              <a>Administrator</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    )
-  
-  }
+      <div className="right-side">
+        <div className="content">
+          <div className="Home">
+            <h2>Home</h2>
+          </div>
+          <div className="View Requests">
+            <h2>View Requests</h2>
+          </div>
+          <div className="Approval">
+            <h2>Approval</h2>
+          </div>
+          <div className="Settings">
+            <h2>Settings</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- website/web-app/src/pages/AdminLogin.tsx
+++ website/web-app/src/pages/AdminLogin.tsx
@@ -9,23 +9,15 @@ export default function AdminLogin() {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div className="login-container" style={{ backgroundColor: 'white', padding: '20px', borderRadius: '10px', width: '300px' }}>
-        <img src={logo} alt="logo" style={{ margin: '20px auto', alignSelf: 'center' }} />
-        <h2>LOGIN</h2>
-        <form>
-          <label>Username:</label>
-          <input
-            type="text"
-            placeholder="Username"
-            style={{ width: '100%', padding: '10px', marginBottom: '10px' }}
-          />
-          <label>Password:</label>
-          <input
-            type="password"
-            placeholder="Password"
-            style={{ width: '100%', padding: '10px', marginBottom: '20px' }}
-          />
-          <button type="submit" style={{ width: '100%', padding: '10px', backgroundColor: '#4CAF50', color: '#fff', border: 'none' }}>Sign In</button>
+      <div className="login-container" style={{ backgroundColor: 'white', padding: '20px', borderRadius: '10px', width: '300px', textAlign: 'center' }}>
+        <img src={logo} alt="logo" style={{ alignSelf: 'center', width: '23pc', height: '21pc', marginBottom: '-80px', marginInlineStart: '-25px', marginTop: '-90px' }} /> 
+        <h2 style={{ marginTop: '15', marginInlineStart: '75px' }}>LOGIN</h2>
+        <form style={{ margin: '10px 0' }}>
+          <label style={{ fontSize: 16, marginBottom: 10, marginInline: '4px', textAlign:'left' }}>Username:</label>&nbsp;
+          <input type="text" style={{ width: '175px', padding: '4px', marginBottom: '20px', backgroundColor: '#F08080', textAlign: 'left', border: 'none', borderRadius: '15px' }} />
+          <label style={{ fontSize: 16, marginBottom: 10, marginInline: '6px', textAlign: 'left' }}>Password:</label>&nbsp;
+          <input type="password" style={{ width: '175px', padding: '4px', marginBottom: '20px', backgroundColor: '#F08080', textAlign: 'left', border: 'none', borderRadius: '15px' }} />
+          <button type="submit" style={{ width: '183px', padding: '5px', marginBottom: '20px', backgroundColor: 'maroon', border: 'none', borderRadius: '15px', color: 'white', fontSize: 16, marginInline: '99px' }}>Sign In</button>
         </form>
       </div>
     </div>
