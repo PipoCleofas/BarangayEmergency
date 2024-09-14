79aa7eaa6fe8256b7e6a5747179263390c0b7f11
PipoCleofas <pipsjacobzxc@gmail.com>
2024-09-14 23:02:13 +0800

    go login

--- hooks/useDataInput.ts
+++ hooks/useDataInput.ts
@@ -395,7 +395,7 @@ const useCheckPassword = () => {
   
       if (id) {
         await AsyncStorage.setItem('id', id.toString());
-        navigation.navigate('index' as never);
+        navigation.navigate('CitizenLogin' as never);
       } else {
         console.error('ID not found in response data');
       }
